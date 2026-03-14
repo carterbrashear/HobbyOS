d1a53f8a6203f0569439a0c6ecd1190739b7e529
Carter Brashear <cbrashear@unr.edu>
2026-03-14 00:48:28 -0700

    Added comments to stage1.asm

--- boot/stage1/stage1.asm
+++ boot/stage1/stage1.asm
@@ -6,21 +6,20 @@ define STAGE2_CODE 0x7e08
 define STAGE2_SIG  29779
 
 start:
-  mov [drive_number], dl
+  mov [drive_number], dl	; We start with the boot drive's number in dl so save it for later
   ;; Set video mode and clear screen
-  xor ah, ah
-  mov al, 0x03
-  int 0x10
+  xor ah, ah				; AH = 0, tells BIOS we are changing the video mode
+  mov al, 0x03				; Video Mode = 3 (80 x 25 text mode)
+  int 0x10					; Request video mode change from the BIOS
   ;; Load next sectors
-  call disk_read
-  mov ax, [STAGE2_ADDR]
-  cmp ax, STAGE2_SIG
-  jne error
-  jmp STAGE2_CODE
-
-error:
-  mov bx, ERROR_MSG
-  call print_string
+  call disk_read			; Load the next sectors of the boot drive
+  mov ax, [STAGE2_ADDR]		; Load the signature at the start of sector 2
+  cmp ax, STAGE2_SIG		; Check to see if sector two loaded correctly
+  jne @f					; If it did not then will jump to our error code
+  jmp STAGE2_CODE			; Runs the code on the next sector of the disk
+@@: ; Error jump point
+  mov bx, ERROR_MSG			; Loads error message string
+  call print_string			; Prints the error message to the screen
 
 ERROR_MSG: db "S1b error!", 0x0a, 0x0d, 0x00
 
